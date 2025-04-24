de1ecf08514f6435defa834db00a57ba9e25a6c5
hari-github1729 <105436908+hari-github1729@users.noreply.github.com>
2025-04-24 08:05:23 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to the Home Page!'
+
+@app.route('/cat')
+def cat():
+    return 'This is the Cat Page!'
+
+@app.route('/dog')
+def dog():
+    return 'This is the Dog Page!'
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port=5000, debug=True)
